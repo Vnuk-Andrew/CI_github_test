01ae5944ae83d265defcb4212129c8ace2b1dbc1
ANDREW-PC\User <vnuk.andrew@gmail.com>
2017-08-19 12:53:51 +0300

    project upload

---
+++ TestCI/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace TestCI
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IWebDriver _browser;
+
+        const string TUTBY = "https://tut.by";
+        const string ONLINER = "https://onliner.by";
+        const string LURKMORE = "https://lurkmo.re";
+
+
+        [OneTimeSetUp]
+        public void ClassInit()
+        {
+            _browser = new ChromeDriver();
+            _browser.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void ClassTearDown()
+        {
+            _browser?.Quit();
+        }
+
+        [Test]
+        public void TestTutBy()
+        {
+            _browser.Navigate().GoToUrl(TUTBY);
+        }
+
+        [Test]
+        public void TestOnliner()
+        {
+            _browser.Navigate().GoToUrl(ONLINER);
+        }
+
+        [Test]
+        public void TestLurkmore()
+        {
+            _browser.Navigate().GoToUrl(LURKMORE);
+        }
+    }
+}
